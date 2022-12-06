f93b5d261a2b9c35f3db33d0c2e7bb4d3042f504
Brian Muriithi M <brianmuriithi71@gmail.com>
2022-12-06 15:49:54 +0300

    Completed lab

---
+++ index.js
@@ -0,0 +1,12 @@
+const divide = () => {
+    return 2000 / 100
+}
+
+console.log(divide(2000,100));
+
+ const square = (c) => c*c
+
+ const add = (d,e) => {
+     return d + e
+ }
+ console.log(add(2,3))
